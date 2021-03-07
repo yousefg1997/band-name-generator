16a4f0a2ce360bddec193f056489a855a9019054
Yousef G <yousefg2032@gmail.com>
2021-03-07 11:07:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# Creating a band generator app.
+# See the example at:
+#   https://band-name-generator-end.appbrewery.repl.run/
+
+print("Welcome to Band Name Generator ⚡")
+city = input("What's the name of the city you grow up in?\n")
+club = input("What's your favorite club name?\n")
+
+print("Your Band Name Could Be: " + club + " " + city)
